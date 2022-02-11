47e597dfdbda25d657509e932fdb7533bff9f5ec
decadenza <decadenza@protonmail.com>
2022-02-11 14:45:15 +0000

    Fixed missing numpy headers

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@
 
 from setuptools import setup, Extension
 
+class get_numpy_include(object):
+    """Defer numpy.get_include() until after numpy is installed."""
+
+    def __str__(self):
+        import numpy
+        return numpy.get_include()
 
 if __name__ == '__main__':
          
@@ -12,12 +18,26 @@ if __name__ == '__main__':
           author_email = "decadenza@protonmail.com",
           url = "http://github.com/decadenza/SimpleStereo",
           packages = ['simplestereo'],
-          ext_modules = [Extension('simplestereo._passive', ['./simplestereo/_passive.cpp']),
-                        Extension('simplestereo._unwrapping', ['./simplestereo/_unwrapping.cpp'])],
+          setup_requires = [
+                            'numpy>=1.19',
+                            ],
           install_requires = [
                              'numpy>=1.19',
                              'opencv-contrib-python>=4.5, <=4.5.3 ',
                              'scipy>=1.4',
                              'matplotlib>=3',
                              ],
+          ext_modules = [
+                        Extension(
+                                'simplestereo._passive', 
+                                ['./simplestereo/_passive.cpp'],
+                                include_dirs=[get_numpy_include()]
+                                ),
+                        Extension(
+                                 'simplestereo._unwrapping',
+                                 ['./simplestereo/_unwrapping.cpp'],
+                                 include_dirs=[get_numpy_include()]
+                                 ),
+                        ],
+          
          )
