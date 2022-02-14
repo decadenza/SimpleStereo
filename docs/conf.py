beaaa96bacfc7c4af76e840512808126489b3496
decadenza <30215028+decadenza@users.noreply.github.com>
2022-02-14 12:34:33 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,13 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [
+                    '_build',
+                    'Thumbs.db',
+                    '.DS_Store',
+                    '.hpp',
+                    '.cpp'
+                    ]
 
 # -- Options for HTML output -------------------------------------------------
 
