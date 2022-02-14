9bab5da3b51965d3ac5c329ece6fd28a30d81e41
decadenza <decadenza@protonmail.com>
2022-02-14 11:54:04 +0000

    docs config

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,9 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
