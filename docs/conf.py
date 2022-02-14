a0f85d8d7fd8f90a9a02f994fb8a06501fb9b547
decadenza <30215028+decadenza@users.noreply.github.com>
2022-02-14 14:16:54 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'cv2', 'scipy.ndimage', 'scipy.optimize', 'scipy.linalg']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'cv2', 'scipy.ndimage', 'scipy.optimize', 'scipy.linalg', '_passive', '_active']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
